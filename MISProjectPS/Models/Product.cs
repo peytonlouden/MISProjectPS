b6e02895ddbe49fd4ef5d9fe63c1bbd8bb7e02b0
Peyton Louden <plouden96@ou.edu>
2019-04-30 20:27:30 -0500

    added some classes that we can use to add products that can be selected by users

---
+++ MISProjectPS/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MISProjectPS.Models
+{
+    public class Product
+    {
+        // this is a data annotation --> describes how to validate user input for that member, to specify formatting for it,
+        //...and to specify how it is modeled when the database is created
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+        public int? CategoryID { get; set; }
+
+        //Not sure what this does but I'll come back to it if I need it
+        public virtual Category Category { get; set; }
+
+    }
+}
---
+++ MISProjectPS/Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MISProjectPS.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("LS HealthWorks")
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
